7c7cfbd60f3ac25e3494f6ff592e7a9c0c30c054
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2023-09-30 09:21:12 +0530

    Create Prims's_algo - MST - adj_mat - connected_weighted_undirected_graph.cpp

---
+++ GRAPH/Prims's_algo - MST - adj_mat - connected_weighted_undirected_graph.cpp
@@ -0,0 +1,26 @@
+int prims(vector<pair<int,int>> adj[],int v){
+    int key[v];
+    fill(key,key+v,INT_MAX);
+    key[0]=0;
+
+    bool mset[v]={false};
+    int res=0;
+
+    for(int i=0;i<v;i++){
+        int u=-1;
+
+        for(int i=0;i<v;i++)
+            if(!mset[i] && ((u==-1) || (key[i] < key[u])))
+                u=i;
+        
+        res+=key[u];
+        mset[u]=true;
+        
+        for(auto x:adj[u])
+            if(!mset[x.first])
+                key[x.first]=min(key[x.first],x.second);
+    }
+    return res;
+}
+//T.C=O(V*(V+E))=>O(V^2)
+//S.C=O(V+E)
