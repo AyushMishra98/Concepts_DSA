3eed91506d93408f4a603ce7898dfae30339ffa6
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2023-12-25 21:17:16 +0530

    Create BFS-cycle_detection_in_undirected_graph.cpp

---
+++ GRAPH/BFS-cycle_detection_in_undirected_graph.cpp
@@ -0,0 +1,32 @@
+bool BFScycle(vector<int> adj[],int s,bool vis[]){
+    vis[s]=true;
+    queue<pair<int,int>> q;
+    q.push({s,-1});
+    while(!q.empty()){
+        int sz=q.size();
+        for(int i=0;i<sz;i++){
+            auto temp=q.front();
+            q.pop();
+            for(auto x:adj[temp.first]){
+                if(x != temp.second && vis[x] == true)
+                    return true;
+                if(x != temp.second){
+                     q.push({x,temp.first});
+                     vis[x]=true;
+                }
+            }
+        }
+    }
+    return false;
+}
+bool BFSdis(vector<int> adj[],int v){
+    bool vis[v+1];
+    memset(vis,false,sizeof(vis));
+    for(int i=0;i<v;i++)
+        for(auto x:adj[i])
+            if(vis[x] == false){
+                if(BFScycle(adj,x,vis))
+                    return true;
+            }
+    return false;
+}
