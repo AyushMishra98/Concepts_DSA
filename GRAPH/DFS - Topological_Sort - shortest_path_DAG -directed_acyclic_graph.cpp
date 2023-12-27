f3a4edb3c648a437055037250274f83b4ad79bf2
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2023-12-27 09:26:14 +0530

    Update DFS - Topological_Sort - shortest_path_DAG -directed_acyclic_graph.cpp

--- GRAPH/DFS - Topological_Sort - shortest_path_DAG -directed_acyclic_graph.cpp
+++ GRAPH/DFS - Topological_Sort - shortest_path_DAG -directed_acyclic_graph.cpp
@@ -1,4 +1,4 @@
-void DFSRec(vector<pair<int,int>> adj[],bool vis[],stack<int>& st,int s){
+    void DFSRec(vector<pair<int,int>>& adj[],vector<bool>& vis,stack<int>& st,int s){
     vis[s]=true;
     
     for(auto x:adj[s])
@@ -7,19 +7,21 @@ void DFSRec(vector<pair<int,int>> adj[],bool vis[],stack<int>& st,int s){
     st.push(s);
     }
     //graph may be disconnected,source may  not be given
-    void dfsTopologicalSort(vector<pair<int,int>> adj[],int v,stack<int>& st){
-        bool vis[v+1];
-        memset(vis,false,sizeof(vis));
-
+    stack<int> dfsTopologicalSort(vector<pair<int,int>> adj[],int v){
+        vector<bool> vis(v,false);
+        
+        stack<int> st;
+        
         for(int i=0;i<v;i++)
             if(vis[i] == false)
                DFSRec(adj,vis,st,i);
+        
+        return st;
     }
     vector<int> shortestPath(vector<pair<int,int>> adj[],int v,int s){
       vector<int> dist(v,INT_MAX);
     
-        stack<int> st;
-        dfsTopologicalSort(adj,v,st);
+        stack<int> st=dfsTopologicalSort(adj,v,st);
 
         dist[s]=0;
         while(!st.empty()){
