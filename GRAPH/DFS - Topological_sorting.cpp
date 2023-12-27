3982e6a2b86f22a77053773654a821275268b0ad
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2023-12-27 09:17:23 +0530

    Update DFS - Topological_sorting.cpp

--- GRAPH/DFS - Topological_sorting.cpp
+++ GRAPH/DFS - Topological_sorting.cpp
@@ -7,11 +7,15 @@ void DFSRec(vector<int> adj[],bool vis[],stack<int> &st,int s){
     st.push(s);
 }
 //graph may be disconnected,source may  not be given
-void dfsTopologicalSort(vector<int> adj[],int v,stack<int>& st){
+stack<int> dfsTopologicalSort(vector<int> adj[],int v){
     bool vis[v+1];
     memset(vis,false,sizeof(vis));
 
+    stack<int> st;
+    
     for(int i=0;i<v;i++)
         if(vis[i] == false)
             DFSRec(adj,vis,st,i);
+    
+    return st;
 }
