cf66facea54e8b36fa3fbb4fdf2b15b3a5f27599
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-08 21:14:16 +0530

    Update Floyd Warshall - all_pair_shortest_path.cpp

--- GRAPH/Floyd Warshall - all_pair_shortest_path.cpp
+++ GRAPH/Floyd Warshall - all_pair_shortest_path.cpp
@@ -1,10 +1,7 @@
 //all pair shortest path - Floyd Warshall algorithm
 void shortest_distance(vector<vector<int>>&m){
+	//non reachable nodes are represented with 1e9 instead of -1
 	    int n=m.size();
-	    for(int i=0;i<n;i++)
-	        for(int j=0;j<n;j++)
-	            if(m[i][j] == -1)
-	                m[i][j]=1e9;
 	                
 	   for(int k=0;k<n;k++){
 	       for(int i=0;i<n;i++){
@@ -13,10 +10,6 @@ void shortest_distance(vector<vector<int>>&m){
 	           }
 	       }
 	   }
-	   for(int i=0;i<n;i++)
-	        for(int j=0;j<n;j++)
-	            if(m[i][j] == 1e9)
-	                m[i][j]=-1;
 	}
 //adjacency matrix representation
 //T.C=O(n^3)
