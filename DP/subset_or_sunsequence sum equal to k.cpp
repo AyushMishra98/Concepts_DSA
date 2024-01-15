ad9f7aa2794f21f639708fa2f13ddda0d398ff38
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-15 11:49:01 +0530

    Create subset_or_sunsequence sum equal to k.cpp

---
+++ DP/subset_or_sunsequence sum equal to k.cpp
@@ -0,0 +1,19 @@
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    vector<vector<bool>> dp(n,vector<bool> (k+1,false));
+    for(int i=0;i<n;i++)
+        dp[i][0]=true;
+    
+    dp[0][arr[0]]=true;
+
+    for(int i=1;i<n;i++){
+        for (int t = 1; t <= k; t++) {
+
+          dp[i][t] = dp[i - 1][t];
+          if (arr[i] <= t)
+            dp[i][t] = dp[i][t] || dp[i - 1][t - arr[i]];
+        }
+    }
+    return dp[n-1][k];
+}
+//T.C=O(N*target)
+//A.S=O(N)
