a638093d16f522d0b1553c7d1bcacc4f27f859d3
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-17 17:56:45 +0530

    Update LCS.cpp

--- DP_on_strings/LCS.cpp
+++ DP_on_strings/LCS.cpp
@@ -42,3 +42,52 @@ int LCS(int idx1,int idx2,string t1, string t2,vector<vector<int>>& dp){
     }
 //T.C=O(n*m)
 //A.S=O(n*m)+O(n+m)
+
+//Tabulation solution
+int longestCommonSubsequence(string t1, string t2) {
+        int n1=t1.length(),n2=t2.length();
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
+        
+        for(int i=0;i<=n1;i++)
+            dp[i][0]=0;
+        
+        for(int i=0;i<=n2;i++)
+            dp[0][i]=0;
+        
+        for(int idx1=1;idx1<=n1;idx1++){
+            for(int idx2=1;idx2<=n2;idx2++){
+                
+                if(t1[idx1-1] == t2[idx2-1])
+                    dp[idx1][idx2]=1+dp[idx1-1][idx2-1];
+                else
+                    dp[idx1][idx2]= max(dp[idx1-1][idx2],dp[idx1][idx2-1]);
+            }
+        }
+        return dp[n1][n2];
+    }
+//T.C=O(n*m)
+//A.S=O(n*m)
+
+//Optimal solution
+int longestCommonSubsequence(string t1, string t2) {
+        int n1=t1.length(),n2=t2.length();
+
+        vector<int> prev(n2+1,0),curr(n2+1,0);
+        
+//         for(int i=0;i<=n2;i++)
+//             prev[i]=0;
+        
+        for(int idx1=1;idx1<=n1;idx1++){
+            for(int idx2=1;idx2<=n2;idx2++){
+                
+                if(t1[idx1-1] == t2[idx2-1])
+                    curr[idx2]=1+prev[idx2-1];
+                else
+                    curr[idx2]= max(prev[idx2],curr[idx2-1]);
+            }
+            prev=curr;
+        }
+        return prev[n2];
+    }
+//T.C=O(n*m)
+//A.S=O(m)
