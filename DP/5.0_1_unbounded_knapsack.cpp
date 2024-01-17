828f5faaaf9fb3d1f08dfd3e7efff353a2c203f4
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-17 11:14:26 +0530

    Create 5.0_1_unbounded_knapsack.cpp

---
+++ DP/5.0_1_unbounded_knapsack.cpp
@@ -0,0 +1,19 @@
+int unboundedKnapsack(int n, int maxW, vector<int> &val, vector<int> &wt){
+    vector<vector<int>> dp(n,vector<int> (maxW+1,0));
+
+	for(int w=wt[0];w<=maxW;w++)
+		dp[0][w]=((int)(w/wt[0]))*val[0];
+
+	for(int i=1;i<n;i++){
+		for(int w=0;w<=maxW;w++){
+			int notTake=dp[i-1][w];
+			int take=INT_MIN;
+			if(wt[i]<= w)
+				take=val[i]+dp[i][w-wt[i]];
+			dp[i][w]=max(take,notTake);
+		}
+	}
+	return dp[n-1][maxW];
+}
+//T.C=O(n*maxWt)
+//A.S=O(maxWt)
