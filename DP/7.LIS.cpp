c15fb3c57f0fe1c175ee89b9728d3ab5b11302b7
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-20 18:25:51 +0530

    Update 7.LIS.cpp

--- DP/7.LIS.cpp
+++ DP/7.LIS.cpp
@@ -7,7 +7,7 @@ int LIS(int idx,int prev_idx,vector<int>& nums){
         if(idx == nums.size())
             return 0;
         
-        int len=LIS(idx+1,idx,nums);
+        int len=LIS(idx+1,prev_idx,nums);
         if(prev_idx == -1 || nums[idx]>nums[prev_idx])
             len=max(len,1+LIS(idx+1,idx,nums));
         return len;
