2c8dbffffa7357fd3a1bdf42812cf13a73f4d47d
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-20 19:09:59 +0530

    Update 7.LIS.cpp

--- DP/7.LIS.cpp
+++ DP/7.LIS.cpp
@@ -15,3 +15,19 @@ int LIS(int idx,int prev_idx,vector<int>& nums){
     int lengthOfLIS(vector<int>& nums) {
         return LIS(0,-1,nums);
     }
+//Tabulation solution
+int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,1);
+        int maxi=1;
+        for(int idx=0;idx<n;idx++){
+            for(int prev_idx=0;prev_idx<idx;prev_idx++){
+                if(nums[prev_idx] < nums[idx])
+                    dp[idx]=max(dp[idx],1+dp[prev_idx]);
+            }
+            maxi=max(maxi,dp[idx]);
+        }
+        return maxi;
+    }
+//T.C=O(n*n)
+//A.S=O(n)
