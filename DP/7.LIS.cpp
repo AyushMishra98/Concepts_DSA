fda3e6b537bf60fa5c9c58d005a1bd786dd5056f
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-22 18:13:40 +0530

    Update 7.LIS.cpp

--- DP/7.LIS.cpp
+++ DP/7.LIS.cpp
@@ -20,12 +20,12 @@ int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         vector<int> dp(n,1);
         int maxi=1;
-        for(int idx=0;idx<n;idx++){
-            for(int prev_idx=0;prev_idx<idx;prev_idx++){
-                if(nums[prev_idx] < nums[idx])
-                    dp[idx]=max(dp[idx],1+dp[prev_idx]);
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(nums[prev] < nums[i])
+                    dp[i]=max(dp[i],1+dp[prev]);
             }
-            maxi=max(maxi,dp[idx]);
+            maxi=max(maxi,dp[i]);
         }
         return maxi;
     }
