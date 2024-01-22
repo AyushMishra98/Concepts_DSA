efa1670f6495b49b4480f76469de2760eacc904c
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-22 09:42:59 +0530

    Create 9.LIS_using_binary_search.cpp

---
+++ DP/9.LIS_using_binary_search.cpp
@@ -0,0 +1,17 @@
+//LIS using Binary Search
+int lengthOfLIS(vector<int>& nums) {
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        
+        for(int i=1;i<nums.size();i++){
+            if(nums[i] > temp.back())
+                temp.push_back(nums[i]);
+            else{
+                auto idx=lower_bound(temp.begin(),temp.end(),nums[i]);
+                temp[idx-temp.begin()]=nums[i];
+            }
+        }
+        return temp.size();
+    }
+//T.C=O(NlogN)
+//A.S=O(N)
