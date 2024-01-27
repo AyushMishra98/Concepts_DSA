5b6bbacd6856910e41ee91549ef17a2c6364e13b
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-01-27 09:20:39 +0530

    Create Largest_area_in_histogram.cpp

---
+++ Array/Largest_area_in_histogram.cpp
@@ -0,0 +1,25 @@
+//T.C=O(N)+O(N)
+//A.S=O(N)
+int largestRectangleArea(vector<int>& h) {
+        int n=h.size();
+        stack<int> s;
+        
+        int maxA=0;
+        for(int i=0;i<=n;i++){
+            while(!s.empty() && (i==n || h[s.top()] >= h[i])){
+                
+                int height=h[s.top()];
+                s.pop();
+                
+                int width;
+                if(s.empty())
+                    width=i-0;
+                else
+                    width=i-s.top()-1;
+                
+                maxA=max(maxA,height*width);
+            }
+            s.push(i);
+        }
+        return maxA;
+    }
