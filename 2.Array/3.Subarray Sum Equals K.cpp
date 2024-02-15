f15f7df72468d924805cc6a4cb0302191090549e
AyushMishra98 <146470718+AyushMishra98@users.noreply.github.com>
2024-02-15 17:29:03 +0530

    Create 3.Subarray Sum Equals K.cpp

---
+++ 2.Array/3.Subarray Sum Equals K.cpp
@@ -0,0 +1,17 @@
+int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> prevsum;
+        
+        int res=0;
+        int currsum=0;
+  
+        for(int i=0;i<nums.size();i++){
+            currsum+=nums[i];
+            if(currsum==k)
+                res++;
+            if(prevsum.find(currsum-k)!=prevsum.end())
+                res+=prevsum[currsum-k];
+            
+            prevsum[currsum]++;
+        }
+        return res;
+    }
